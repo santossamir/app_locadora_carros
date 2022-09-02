73e8490951aeb259cac2fd86ed225f0f7f60979e
Samir Santos <santossamir@hotmail.com>
2022-09-02 16:00:44 -0300

    Primeiro commit, desenvolvi controllers, migrations, models e factories.

---
+++ database/migrations/2022_09_02_164703_create_carros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_carros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('placa', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
+            $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('modelo_id')->references('id')->on('tb_modelos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_carros');
+    }
+}
