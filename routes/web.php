8d8d90f48c3586eea03565cb370fb6176f6d63d0
Samir Santos <santossamir@hotmail.com>
2022-10-18 16:15:07 -0300

    Configurando o Vue.js e criando o component vue Login.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\ClienteController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
